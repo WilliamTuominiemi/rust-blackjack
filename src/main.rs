f9e225c6b84630da5bce16de3d8c4eb3bf9921b0
Tuominiemi William <william.tuominiemi@aalto.ficd>
2025-06-19 21:56:18 +0300

    enhance dealing with blackjack logic

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,42 @@ fn main() {
     let mut numbers: Vec<i32> = (1..13).collect();
     numbers.shuffle(&mut rng);
 
-    deal_card(suits, numbers.clone(), rng.clone());
-    deal_card(suits, numbers.clone(), rng.clone());
+    let mut total = 0;
+
+    total += deal_card(total);
+
+    println!("You're at {}", total);
+
+    total += deal_card(total);
+
+    println!("You're at {}", total);
 }
 
-fn deal_card(suits: [char; 4], numbers: Vec<i32>, mut rng: rand::prelude::ThreadRng) {
+fn deal_card(total: i32) -> i32 {
+    let card_number = get_card();
+
+    if card_number == 1 && (total - 11) < 21 {
+        return 11;
+    } else if card_number == 11 || card_number == 12 || card_number == 13 {
+        return 10;
+    } else {
+        return card_number;
+    }
+}
+
+fn get_card() -> i32 {
+    let mut rng: rand::prelude::ThreadRng = rand::thread_rng();
+
+    let mut suits: [char; 4] = ['C', 'D', 'H', 'S'];
+    suits.shuffle(&mut rng);
+
+    let mut numbers: Vec<i32> = (1..13).collect();
+    numbers.shuffle(&mut rng);
+
     let suit = suits.choose(&mut rng).unwrap();
     let number = numbers.choose(&mut rng).unwrap();
 
     println!("{} {}", suit, number);
+
+    return *number;
 }
